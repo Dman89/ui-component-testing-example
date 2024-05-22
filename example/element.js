aab54a99444ffa1fbb4f4fd76753502a1e35f940
Dman89 <Daniel@DanielCudney.com>
2024-05-22 16:23:54 -0700

    Baseline

---
+++ example/element.js
@@ -0,0 +1,8 @@
+import '../src/cadso-ui-component-test-example';
+
+const el = document.createElement('DIV');
+document.body.appendChild(el);
+
+el.innerHTML = `		
+<cadso-ui-component-test-example></cadso-ui-component-test-example>
+`;
